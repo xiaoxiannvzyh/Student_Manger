8942f2a25a75ea48840ee4ea6ac27167229f2e2c
LAPTOP-VMIJDT2P\li hanyu <1328411791@qq.com>
2022-06-17 00:01:30 +0800

    fix:一些内容，增加测试类

--- readme.md
+++ readme.md
@@ -1,1 +1,4 @@
 ### 介绍
+一个练手学生管理系统
+
+使用 Spring +mybatis 开发+java Swing界面
--- src/test/java/org/lihanyu/service/UserServiceTest.java
+++ src/test/java/org/lihanyu/service/UserServiceTest.java
@@ -3,6 +3,7 @@ package org.lihanyu.service;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.lihanyu.config.SpringConfig;
+import org.lihanyu.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -17,6 +18,13 @@ public class UserServiceTest {
     @Test
     public void deleteUser() {
         userService.delete(1);
+        System.out.println(userService.selectAll());
+    }
+
+    @Test
+    public void addeUser() {
+        User user = new User(123, "112", "111", "111");
+        userService.addUser(user);
         System.out.println(userService.selectAll());
     }
 
