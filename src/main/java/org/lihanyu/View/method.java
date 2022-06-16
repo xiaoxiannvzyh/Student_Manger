d7b22a2dfa0a8b1ddbac4111639fd68b87531dff
a2290773276 <96385515+a2290773276@users.noreply.github.com>
2022-06-16 12:52:58 +0800

    feature:基础界面

--- src/main/java/org/lihanyu/View/method.java
+++ src/main/java/org/lihanyu/View/method.java
@@ -17,8 +17,8 @@ public class method {
     public Object showDetail(Object row[][]) {
         List<User> users=userService.selectAll();
         for (int i = 0; i < users.size(); i++) {
-            row[i][0] = users.get(i).getId();
-            row[i][1] = users.get(i).getName();
+            row[i][0] = users.get(i).getName();
+            row[i][1] = users.get(i).getId();
             row[i][2] = users.get(i).getEmail();
             row[i][3] = users.get(i).getPhone();
         }
