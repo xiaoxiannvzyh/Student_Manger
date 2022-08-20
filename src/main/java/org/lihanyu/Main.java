446bbe504672c9729e8358a894be22a2b35177b1
LAPTOP-VMIJDT2P\li hanyu <1328411791@qq.com>
2022-08-20 21:44:40 +0800

    增加输出

--- src/main/java/org/lihanyu/Main.java
+++ src/main/java/org/lihanyu/Main.java
@@ -8,6 +8,7 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Hello World!");
         LoginFrame lf = new LoginFrame();
         lf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
